e5bb62e6c3ae7a8b8eaf23ba9f6030d84707d1cd
Matt Carrier <mcarrieruri@gmail.com>
2014-02-10 22:17:40 -0500

    Move to meteor.

--- js/squares.js
+++ js/squares.js
@@ -1,14 +1,33 @@
-$(document).ready(function () {
-	$('#lgrid').append('<div class=\"height-11\"></div>');
-	for (var i=0; i<10; i++) {
-		$('#lgrid').append('<div class=\"height-11 padded\"><div class=\"bbox fullheight\"></div></div>');
-	}
+if (Meteor.isClient) {
+  // Template.hello.greeting = function () {
+  //   return "Welcome to squares.";
+  // };
 
-	for (var i=0; i<10; i++) {
-		$('#rugrid').append('<div class=\"bit-10 padded fullheight\"><div class=\"bbox fullheight\"></div></div>');
-	}
+  // Template.hello.events({
+  //   'click input' : function () {
+  //     // template data, if any, is available in 'this'
+  //     if (typeof console !== 'undefined')
+  //       console.log("You pressed the button");
+  //   }
+  // });
 
-	for (var i=0; i<100; i++) {
-		$('#mgrid').append('<div class=\"bit-10 padded height-11\"><div class=\"mbox fullheight\"></div></div>');
-	}
-});
+  Meteor.startup(function () {
+  	for (var i=0; i<10; i++) {
+  		$('#lgrid').append('<div class=\"height-11 padded\"><div class=\"bbox fullheight\"></div></div>');
+  	}
+
+  	for (var i=0; i<10; i++) {
+  		$('#rugrid').append('<div class=\"bit-10 padded fullheight\"><div class=\"bbox fullheight\"></div></div>');
+  	}
+
+  	for (var i=0; i<100; i++) {
+  		$('#mgrid').append('<div class=\"bit-10 padded height-11\"><div class=\"mbox fullheight\"></div></div>');
+  	}
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
